7e63ebe0247725258837628742e9fd950f703b1c
Jihun Park <jihunpark0989@gmail.com>
2023-05-20 17:21:13 +0100

    local db connection

--- dataAccess.php
+++ dataAccess.php
@@ -5,11 +5,11 @@ class DataAccess
     private $pdo;
     private function __construct()
     {
-        $db_name = "landingpage";
+        $db_name = "email-list";
         $db_user = "root";
         $db_password = "";
         $this->pdo = new 
-        PDO("mysql:host=localhost;dbname=$db_name",$db_user,$db_password, [PDO::ATTR_ERRMODE=> PDO:: ERRMODE_EXCEPTION]);
+            PDO("mysql:host=localhost;dbname=$db_name",$db_user,$db_password, [PDO::ATTR_ERRMODE=> PDO:: ERRMODE_EXCEPTION]);
     }
     public static function getInstance()
     {
@@ -21,14 +21,14 @@ class DataAccess
     }
     function getAllEmailList()
     {
-        $statement = $this->pdo->prepare("SELECT * FROM emaillist");
+        $statement = $this->pdo->prepare("SELECT * FROM email-list");
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_CLASS, "EmailList");
         return $result;
     }
     function addToEmailList($newEmail) //emailList object
     {
-        $sql ="INSERT INTO emaillist (id, name, email, wantUpdate, happy) VALUES (null, ?, ?, ?, ?);";
+        $sql ="INSERT INTO email-list (id, name, email, wantUpdate, happy) VALUES (null, ?, ?, ?, ?);";
         $statement = $this->pdo->prepare($sql);
         $email= $newEmail->email;
         $name =$newEmail->name;
